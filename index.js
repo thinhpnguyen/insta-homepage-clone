aebe68bf16fe9ca38630859b0183b7872c3daa98
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2022-08-15 13:38:11 -0700

    use requestAnimatonFrame for the animation instead

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 (function main() {
   let currImage = document.getElementById("1");
   let prev = document.getElementById("1");
-  setTimeout(() => {
-    setInterval(() => {
-      let next = document.getElementById(currImage.dataset.next);
 
+  let prevTime;
+  const interval = 5000;
+
+  function step(timestamp) {
+    if (prevTime === undefined) prevTime = timestamp;
+    const elapsed = timestamp - prevTime;
+    if (elapsed >= interval) {
       //only two images will be visible at a time
       //previous image will fade out
       //while new one will fade in
-
+      const next = document.getElementById(currImage.dataset.next);
       prev.classList.remove("last-active");
       currImage.classList.replace("active", "last-active"); //used for fade out
       prev = currImage;
       currImage = next;
       currImage.classList.add("active");
-    }, 5000);
+      prevTime = timestamp;
+    }
+    window.requestAnimationFrame(step);
+  }
+
+  //wait a while before starting to show different images
+  setTimeout(() => {
+    window.requestAnimationFrame(step);
   }, 2000);
 })();
